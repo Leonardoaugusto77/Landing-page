ebbf7dd38d4b7e5b99e1b024c86c4125a618a2b9
Heisen <leonardopjh30@gmail.com>
2022-11-16 21:07:02 -0300

    updates

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var btnS = document.getElementById("sub")
 
+var inputN = document.getElementById("inputN")
+
 btnS.addEventListener('click', ()=>{
+    if(inputN.value === ''){
+        window.alert("PRENCHA POR FAVOR!")
+        return
+    }
+
     window.alert('Formulario enviado com sucesso, Check seu email!')
     return
 })
